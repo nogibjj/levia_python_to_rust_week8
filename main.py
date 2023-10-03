fb0b819ce1e7db8cb68bce7801f34fbbb3a68550
Levia Yang <73449544+xxxtesttest@users.noreply.github.com>
2023-10-03 06:08:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 # main.py
 
-import sqlite3
+# import sqlite3
 
-def create_table():
-    connection = sqlite3.connect('test.db')
-    cursor = connection.cursor()
-    cursor.execute("""CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)""")
-    connection.commit()
-    connection.close()
+# def create_table():
+#     connection = sqlite3.connect('test.db')
+#     cursor = connection.cursor()
+#     cursor.execute("""CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)""")
+#     connection.commit()
+#     connection.close()
 
-def insert_data(name, age):
-    connection = sqlite3.connect('test.db')
-    cursor = connection.cursor()
-    cursor.execute('INSERT INTO users (name, age) VALUES (?, ?)', (name, age))
-    connection.commit()
-    connection.close()
+# def insert_data(name, age):
+#     connection = sqlite3.connect('test.db')
+#     cursor = connection.cursor()
+#     cursor.execute('INSERT INTO users (name, age) VALUES (?, ?)', (name, age))
+#     connection.commit()
+#     connection.close()
 
-def read_data():
-    connection = sqlite3.connect('test.db')
-    cursor = connection.cursor()
-    result = cursor.execute('SELECT * FROM users')
-    data = result.fetchall()
-    connection.close()
-    return data
+# def read_data():
+#     connection = sqlite3.connect('test.db')
+#     cursor = connection.cursor()
+#     result = cursor.execute('SELECT * FROM users')
+#     data = result.fetchall()
+#     connection.close()
+#     return data
