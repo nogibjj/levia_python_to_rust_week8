df03db585e5346b22786595b5c352e2789f444e6
Levia Yang <73449544+xxxtesttest@users.noreply.github.com>
2023-10-15 23:55:23 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 // main.rs
-use my_project::database::DbConnection;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let database_url = "your_database_url_here";
-    let connection = DbConnection::new(database_url).await?;
+use my_project::Database;
 
-    let data = connection.get_data().await?;
-    
-    // Process the data as needed
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let db = Database::new()?;
+    let data = db.get_all_data()?;
+
+    for item in data {
+        println!("{}", item);
+    }
 
     Ok(())
 }
