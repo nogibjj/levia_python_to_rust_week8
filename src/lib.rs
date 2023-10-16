3d9dc82b5689513c7db8031295c4f4364209cf5b
Levia Yang <73449544+xxxtesttest@users.noreply.github.com>
2023-10-15 23:46:03 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use rusqlite::{Connection, Result};
+use rusqlite::{Connection, Result, ToSql};
+use std::convert::TryFrom;
 
 pub struct Database {
     conn: Connection,
@@ -13,42 +14,39 @@ impl Database {
                 name TEXT,
                 email TEXT
             )",
-            [] 
+            [],
         )?;
         Ok(Database { conn })
     }
 
-    
-
     pub fn insert_user(&self, name: &str, email: &str) -> Result<()> {
         self.conn.execute(
             "INSERT INTO users (name, email) VALUES (?1, ?2)",
-            &[name, email]
+            &[name, email],
         )?;
         Ok(())
     }
 
-
-    pub fn get_users(&self) -> Result<Vec<(i64, String, String)>> {
+    pub fn get_users(&self) -> Result<Vec<(i64, String, String)>>
+    {
         let mut stmt = self.conn.prepare("SELECT id, name, email FROM users")?;
-        let user_iter = stmt.query_map([], |row| Ok((row.get(0)?, row.get(1)?, row.get(2)?)))?;
+        let user_iter = stmt.query_map([], |row| {
+            Ok((
+                row.get(0)?,
+                row.get(1)?,
+                row.get(2)?,
+            ))
+        })?;
 
-        let mut users = Vec::new();
-        for user in user_iter {
-            users.push(user?);
-        }
-        Ok(users)
+        let users: rusqlite::Result<Vec<(i64, String, String)>> = user_iter.collect();
+        Ok(users?)
     }
 
-
     pub fn update_user(&self, id: i64, new_email: &str) -> Result<()> {
-    self.conn.execute(
-        "UPDATE users SET email = ?1 WHERE id = ?2",
-        &[new_email as &dyn rusqlite::ToSql, &id as &dyn rusqlite::ToSql]
-    )?;
-    Ok(())
-}
-
-
-
+        self.conn.execute(
+            "UPDATE users SET email = ?1 WHERE id = ?2",
+            &[new_email as &dyn ToSql, &id],
+        )?;
+        Ok(())
+    }
 }
